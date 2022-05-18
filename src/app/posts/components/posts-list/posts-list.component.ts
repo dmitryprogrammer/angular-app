120e7ffbe60c4cba91b67d99c0af2bcdde0f5a1e
dpischalka <dpischlka@gmail.com>
2022-05-19 01:05:01 +0400

    [main] Post lists, Posts.

--- src/app/posts/components/posts-list/posts-list.component.ts
+++ src/app/posts/components/posts-list/posts-list.component.ts
@@ -1,20 +1,48 @@
-import {Component} from "@angular/core";
-import {Observable} from "rxjs";
+import {Component, OnDestroy, OnInit} from "@angular/core";
+import {filter, Observable, Subject, takeUntil} from "rxjs";
 
-import {Posts} from "../../../core/models/posts.models";
+import {Post, Posts} from "../../../core/models/posts.models";
 import {PostsService} from "../../../core/services/posts.service";
 import {trackByFn} from "../../../core/utils/util";
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: "app-posts-list",
   templateUrl: "./posts-list.component.html",
   styleUrls: ["./posts-list.component.scss"]
 })
-export class PostsListComponent {
-  public posts$: Observable<Posts> = this.postsService.getPosts();
+export class PostsListComponent implements OnInit, OnDestroy {
+  private unsubscribeSubject$: Subject<null> = new Subject<null>();
+  private posts$: Observable<Posts> = this.postsService.getPosts();
+  private initialPosts: Posts = [];
+  public posts: Posts = [];
+  public form: FormGroup = this.fb.group({
+    title: [""]
+  });
+  public trackByFn = trackByFn;
 
-  constructor(private postsService: PostsService) {
+  constructor(private fb: FormBuilder, private postsService: PostsService) {
   }
 
-  public trackByFn = trackByFn;
+  public ngOnInit(): void {
+    this.posts$.pipe(takeUntil(this.unsubscribeSubject$)).subscribe((posts: Posts) => {
+      this.posts = posts;
+      this.initialPosts = posts;
+    });
+
+    this.form.valueChanges
+      .pipe(
+        filter(() => Boolean(this.initialPosts?.length)),
+        takeUntil(this.unsubscribeSubject$)
+      )
+      .subscribe(({title}: {title: string; type: string}) => {
+        const valueTitle = title.toLowerCase().trim();
+        this.posts = title ? this.posts.filter(({title: postTitle}: Post) => postTitle.includes(valueTitle)) : this.initialPosts;
+      });
+  }
+
+  public ngOnDestroy(): void {
+    this.unsubscribeSubject$.next(null);
+    this.unsubscribeSubject$.complete();
+  }
 }
--- src/app/posts/posts.module.ts
+++ src/app/posts/posts.module.ts
@@ -6,9 +6,10 @@ import {PostsListComponent} from "./components/posts-list/posts-list.component";
 import {PostsRoutingModule} from "./posts-routing.module";
 import {PostComponent} from "./components/post/post.component";
 import {CoreModule} from "../core/core.module";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
-  imports: [CommonModule, RouterModule, PostsRoutingModule, CoreModule],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule, PostsRoutingModule, CoreModule],
   declarations: [PostsListComponent, PostComponent]
 })
 export class PostsModule {
